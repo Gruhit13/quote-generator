f87ad5e706f52559c4a1a5bb1257f5a50a95ed74
Gruhit13 <gruhitspatel15@gmail.com>
2024-04-09 21:15:30 -0400

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
 
-request_url = 'http://127.0.0.1:8000'
+# request_url = 'http://127.0.0.1:8000'
+request_url = 'https://gruhit13-quote-generator-backend.hf.space'
 
 # Main title of the page
 st.title('Quote Generator 🤖')
