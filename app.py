1eb9f710fa39a0ed05d6b6128804741a1eadee34
Gruhit13 <gruhitspatel15@gmail.com>
2024-04-10 16:28:27 -0400

    Added API Token for making API calls

--- app.py
+++ app.py
@@ -46,7 +46,6 @@ def get_quote():
 
         # The response would be <startoftext>tags<bot>:AI response
         # so we just need to take the AI respones
-        st.write(response_data)
         st.session_state['quote'] = response_data['quote'].split('<bot>:')[-1]
 
 
